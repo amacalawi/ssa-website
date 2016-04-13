6d541f6217505a127f2ed7716535c3eb00dbdfb3
John Lioneil Dionisio <john.dionisio1@gmail.com>
2016-04-13 10:48:21 +0800

    added favicons

--- wp-content/themes/ssa-three-zero/functions.php
+++ wp-content/themes/ssa-three-zero/functions.php
@@ -6,6 +6,38 @@ $locale = get_locale();
 $locale_file = TEMPLATEPATH . "/languages/$locale.php";
 if ( is_readable( $locale_file ) ) require_once( $locale_file );
 
+# Favicons
+add_action( 'login_head', 'add_favicon' );  # login screen
+add_action( 'wp_head', 'add_favicon' );     # front end
+add_action( 'admin_head', 'add_favicon' );  # admin end
+function add_favicon() { ?>
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo bloginfo('url') ?>/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo bloginfo('url') ?>/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo bloginfo('url') ?>/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo bloginfo('url') ?>/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo bloginfo('url') ?>/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo bloginfo('url') ?>/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo bloginfo('url') ?>/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo bloginfo('url') ?>/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo bloginfo('url') ?>/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="<?php echo bloginfo('url') ?>/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="<?php echo bloginfo('url') ?>/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="<?php echo bloginfo('url') ?>/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="<?php echo bloginfo('url') ?>/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="<?php echo bloginfo('url') ?>/manifest.json">
+    <link rel="mask-icon" href="<?php echo bloginfo('url') ?>/safari-pinned-tab.svg" color="#ffbf3d">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-TileImage" content="<?php echo bloginfo('url') ?>/mstile-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]--><?php
+}
+
 // Clean up the WordPress Head
 if( !function_exists( "wp_bootstrap_head_cleanup" ) ) {
   function wp_bootstrap_head_cleanup() {
